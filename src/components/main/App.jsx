a3abd969ec3ce467c0fd0067984db731fed0d905
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-22 14:52:34 -0300

    feat: create sctructure and fix bugs

---
+++ src/components/main/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+export default (props) => (
+  <BrowserRouter>
+    <div className="app">
+      <h1>Oi</h1>
+    </div>
+  </BrowserRouter>
+);
