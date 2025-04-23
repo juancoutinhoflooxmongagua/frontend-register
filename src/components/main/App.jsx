d4c2f1c3b839d20c6b22b05a96635175b8976c88
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-23 11:23:30 -0300

    feat: finish project and fix bugs

--- src/components/main/App.jsx
+++ src/components/main/App.jsx
@@ -4,21 +4,23 @@ import Footer from "../template/Footer";
 import Header from "../template/Header";
 import UserCrud from "../user/UserCrud";
 
-export default (props) => (
-    <div className="container-fluid">
-      <div className="row">
-        <aside className="col-md-2 bg-light p-3">
-          <Logo />
-        </aside>
-        <main className="col-md-10">
-          <Header />
-          <UserCrud></UserCrud>
-        </main>
-      </div>
-      <footer className="row">
-        <div className="col-12">
-          <Footer />
+export default () => (
+  <div className="container-fluid">
+    <div className="row">
+      <aside className="col-md-2 bg-light p-3">
+        <Logo />
+      </aside>
+      <main className="col-md-10">
+        <Header />
+        <div className="d-flex justify-content-center">
+          <UserCrud />
         </div>
-      </footer>
+      </main>
     </div>
+    <footer className="row">
+      <div className="col-12">
+        <Footer />
+      </div>
+    </footer>
+  </div>
 );
